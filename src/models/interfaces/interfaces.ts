b76117389cb4e75a09b75c10a8c05693776c82f2
BohdanLytvynov <blitvinov19@gmail.com>
2025-07-09 10:00:37 +0200

    updating branch

--- src/models/interfaces/interfaces.ts
+++ src/models/interfaces/interfaces.ts
@@ -1,8 +1,12 @@
+export interface EntityBase{
+    id : number;
+}
 
-export interface CPU {
+export interface ICPU extends EntityBase {
     brand : string;
+    name : string;
     clock_frequency : number;
     cache_memory_size : number;
     number_of_cores : number;
-    bit_depth : number;
+    bit_depth : number;    
 }
--- src/models/models.ts
+++ src/models/models.ts
@@ -1,12 +1,31 @@
-import {CPU as ICPU} from "../models/interfaces/interfaces"
+import {ICPU} from "../models/interfaces/interfaces"
+
+export enum CPU_Fields{
+    Brand = 0, Name, Clock_Frequency, Cache_Memory_Size, Number_Of_Cores, Bit_Depth
+}
 
 export class CPU implements ICPU{
     
-    constructor(public brand : string, 
+    constructor(public id : number,
+        public brand : string, 
+        public name : string,
         public clock_frequency : number,
         public cache_memory_size : number,
         public number_of_cores : number,
         public bit_depth : number,
     )
     {}
+
+    static Init() : CPU{
+        return new CPU(-1,'','',0,0,0,0);
+    }
+
+    public Update(src : CPU) : void{
+        this.name = src.name;
+        this.brand = src.brand;
+        this.number_of_cores = src.number_of_cores;
+        this.clock_frequency = src.clock_frequency;
+        this.cache_memory_size = src.cache_memory_size;
+        this.bit_depth = src.bit_depth;
+    }
 }
